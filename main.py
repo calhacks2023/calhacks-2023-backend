ab4a93ab0df3f4126b135fa9dc77f2f0de35cffc
William Hou <whou125@gmail.com>
2023-10-28 11:04:11 -0700

    Add route skeletons

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
+from models import RecipeAndRestrictions
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/submitrecipe")
+async def submit_recipe(recipe: RecipeAndRestrictions):
+    return {'recipe': recipe.recipe, 'restrictions': recipe.restrictions}
+    
 
 # For deployment on Render
 if __name__ == '__main__':
