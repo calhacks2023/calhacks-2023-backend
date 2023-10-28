a68ea1d7cf71fc82117516e11aa78e0729abf23d
Ruslan Manoharan <57958158+Ru23-GitHub@users.noreply.github.com>
2023-10-28 12:19:00 -0700

    setup basic response handling

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
 
+# Configure CORS to allow requests from your React application's origin
+app.add_middleware(CORSMiddleware,allow_origins=["http://localhost:5173"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"],)
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    #return {"message": "Hello World"}
+    param1 = request.query_params.get('param1')
+    param2 = request.query_params.get('param2')
+
+    print("WE GOT:",param1, param2)
+
+
+    # Use param1 and param2 as needed
+    return {"message": f"Received param1: {param1}, param2: {param2}"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=10000)
