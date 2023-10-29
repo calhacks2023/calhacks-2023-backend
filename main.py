0ce3ca63f24d95e68769ef783471024c34d447ea
William Hou <whou125@gmail.com>
2023-10-29 01:27:15 -0700

    Prepare for prod deployment

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ openai.api_key = OPENAI_API_KEY
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
 # Configure CORS to allow requests from your React application's origin
-app.add_middleware(CORSMiddleware,allow_origins=["http://localhost:5173"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"],)
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"],)
 
 @app.get("/")
 async def root(request: Request):
@@ -26,13 +30,19 @@ async def root(request: Request):
 
 @app.post("/submitrecipe")
 async def submit_recipe(recipe: RecipeAndRestrictions):
+    
+    print("sussy")
+    print(recipe.recipe)
+    print(recipe.restrictions)
     completion = openai.ChatCompletion.create(
     model="gpt-4",
     messages=[
         {"role": "system", "content": "You are an application that takes in an ingredient list of ingredients for a recipe, as well as a list of allergens, and will modify the ingredients by taking out the allergy-containing ingredient and replacing it with a viable substitution. Be sure to consider what the recipe is for and if it is sweet or savory when deciding on a viable replacement (for example apple sauce is a bad egg replacment for savory dishes but good for sweet dishes). When deciding on a viable replacement, weigh options what are more common or accessible for general use more favorably over pther potential solutions. Results will be in a JSON format, being {{'ingredients':'INGREDIENT,INGREDIENT,INGREDIENT...','changes':<EXPLANATION HERE>}}. The ingredients should be displayed in a list, and the modified ingredients should be italicized. Only italicize the ingredients we are using as substitutions, it should not be italisized if it was mentioned in the original inputted recipe. For example, if the original recipe includes flour,chicken, and we replace the chicken with mushrooms, then the output would only italicize mushrooms (as *mushrooms*) but not the flour. This list will be in the 'ingredients' part of the JSON object.You will take in input by first reading in a recipe list and below it will be a string 'Allergies:' followed by a list of allergens. Then, you should explain what potential effects the substitution could have on the food item. For example, if eggs are replaced with bananas in a cookie recipe, then the cookies may be sweeter than usual. Place this explanation string inside the 'changes' section in the JSON object. Overall the output will be the JSON object which has the numbered modified ingredients list and an explanation of what potential changes may occur in the recipe with the modifications."}
         ,{"role": "user", "content": f"{recipe.recipe}\nAllergies: {recipe.restrictions}"}]
     )
-    #print(completion.choices[0].message["content"])
+    
+    print("among us")
+    print(completion.choices[0].message["content"])
 
     json_object = json.loads(completion.choices[0].message["content"])    
     return json_object
