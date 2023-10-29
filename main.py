1f6588ecf1313404a3bf5c4e01402d289b7b58a9
Ruslan Manoharan <57958158+Ru23-GitHub@users.noreply.github.com>
2023-10-28 22:31:08 -0700

    Implemented receiving post from f.end and returning our info via gpt4

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.testclient import TestClient
 import uvicorn
 from models import RecipeAndRestrictions
+import openai
+import APIKEY #place your api key in a seperate APIKEY.py file as object 'api_key'
+
+OPENAI_API_KEY = APIKEY.api_key # @RU REMOVE BEFORE COMMITING!!! AHHHHHH
+openai.api_key = OPENAI_API_KEY
 
 app = FastAPI()
 
@@ -11,19 +17,35 @@ app.add_middleware(CORSMiddleware,allow_origins=["http://localhost:5173"],allow_
 @app.get("/")
 async def root(request: Request):
     #return {"message": "Hello World"}
-    param1 = request.query_params.get('param1')
-    param2 = request.query_params.get('param2')
 
-    print("WE GOT:",param1, param2)
 
 
     # Use param1 and param2 as needed
-    return {"message": f"Received param1: {param1}, param2: {param2}"}
+    return {"message":f"hewo"}
 
 @app.post("/submitrecipe")
 async def submit_recipe(recipe: RecipeAndRestrictions):
-    return {'recipe': recipe.recipe, 'restrictions': recipe.restrictions}
-    
+    completion = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are an application that takes in an ingredient list of ingredients for a recipe, as well as a list of allergens, and will modify the ingredients by taking out the allergy-containing ingredient and replacing it with a viable substitution. Be sure to consider what the recipe is for and if it is sweet or savory when deciding on a viable replacement (for example apple sauce is a bad egg replacment for savory dishes but good for sweet dishes). When deciding on a viable replacement, weigh options what are more common or accessible for general use more favorably over pther potential solutions. Results will be in a JSON format, being {{'ingredients':'INGREDIENT,INGREDIENT,INGREDIENT...','changes':<EXPLANATION HERE>}}. The ingredients should be displayed in a list, and the modified ingredients should be italicized. Only italicize the ingredients we are using as substitutions, it should not be italisized if it was mentioned in the original inputted recipe. For example, if the original recipe includes flour,chicken, and we replace the chicken with mushrooms, then the output would only italicize mushrooms (as *mushrooms*) but not the flour. This list will be in the 'ingredients' part of the JSON object.You will take in input by first reading in a recipe list and below it will be a string 'Allergies:' followed by a list of allergens. Then, you should explain what potential effects the substitution could have on the food item. For example, if eggs are replaced with bananas in a cookie recipe, then the cookies may be sweeter than usual. Place this explanation string inside the 'changes' section in the JSON object. Overall the output will be the JSON object which has the numbered modified ingredients list and an explanation of what potential changes may occur in the recipe with the modifications."}
+        ,{"role": "user", "content": f"{recipe.recipe}\nAllergies: {recipe.restrictions}"}]
+    )
+    #print(completion.choices[0].message["content"])
+    return completion.choices[0].message["content"]
+
+
+
+# client = TestClient(app) #TESTING
+
+# data = {
+#     "recipe": "2 slices of wheat bread, mayonaise, 4 strips of bacon, 2 slices of roma tomato, lettuce",
+#     "restrictions": "Egg"
+# }
+
+# response = client.post("/submitrecipe", json=data)
+
+# print(response.json())
 
 # For deployment on Render
 if __name__ == '__main__':
